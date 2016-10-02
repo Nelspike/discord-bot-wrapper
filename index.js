fe4c19d1618168a1d6c0b5eab242ccedae63e915
Nelspike <nelspiketx@gmail.com>
2016-10-02 16:55:04 +0200

    Change Bot interface

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
 'use strict';
 
-const _ = require('lodash');
-const assert = require('assert');
-
-module.exports = options => {
-  assert(options.name, 'Your bot must have a name!');
-  assert(options.auth, 'Your bot must have authentication means!');
-  assert(options.events, 'Your bot must have events!');
-
-  const botOptions = {
-    name: options.name,
-    revive: options.revive,
-  };
-
-  _.keys(options.auth).forEach(key => {
-    botOptions[key] = options.auth[key];
-  });
-
-  const bot = new require('./lib/Bot')(botOptions);
-
-  _.keys(options.events).forEach(key => {
-    bot.setEvent(key, options.events[key]);
-  });
-
-  bot.login();
-
-  return bot;
+/**
+ * Exports for the discord-bot-wrapper.
+ * @returns {Object} An object that includes the `init` function, as well as the `Bot` class.
+ */
+module.exports = {
+  init: require('./lib/init'),
+  Bot: require('./lib/Bot'),
 };
